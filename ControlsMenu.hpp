e96c8ecc939692b1391c1b4fff3ca5e7f3591241
matteoJeulin <matteo.jeulin@gmail.com>
2025-12-04 23:16:04 -0500

    New cutscene menus

--- ControlsMenu.hpp
+++ ControlsMenu.hpp
@@ -13,12 +13,20 @@ struct Button;
 
 struct ControlsMenu : Mode
 {
-    ControlsMenu(std::shared_ptr<Mode> _nextMode);
+    enum Type
+    {
+        Controls,
+        Level1Cutscene,
+        Level2Cutscene
+    };
+
+    ControlsMenu(std::shared_ptr<Mode> _nextMode, Type type);
 
     // functions called by main loop:
     virtual bool handle_event(SDL_Event const &, glm::uvec2 const &window_size) override;
     virtual void update(float elapsed) override;
     virtual void draw(glm::uvec2 const &drawable_size) override;
+
 
     std::vector<Button> buttons;
     static std::vector<UIElement> controls;
